b68d6a31cb184ab976d9d300813088044d25aeca
王滔 <wwtdsg@gmail.com>
2015-05-09 20:03:26 +0800

    Update PhoneNumber.cpp

--- PhoneNumber.cpp
+++ PhoneNumber.cpp
@@ -19,19 +19,19 @@ int conver(char ch)
 {
 	if(ch=='A' || ch=='B' || ch=='C')  
         return 2;  
-    if(ch=='D' || ch=='E' || ch=='F')  
+        if(ch=='D' || ch=='E' || ch=='F')  
         return 3;  
-    if(ch=='G' || ch=='H' || ch=='I')  
-        return 4;  
-    if(ch=='J' || ch=='K' || ch=='L')  
-        return 5;  
-    if(ch=='M' || ch=='N' || ch=='O')  
-        return 6;  
-    if(ch=='P' || ch=='R' || ch=='S')  
-        return 7;  
-    if(ch=='T' || ch=='U' || ch=='V')  
-        return 8;  
-    if(ch=='W' || ch=='X' || ch=='Y')  
+        if(ch=='G' || ch=='H' || ch=='I')  
+        return 4;
+        if(ch=='J' || ch=='K' || ch=='L')  
+        return 5;
+        if(ch=='M' || ch=='N' || ch=='O')  
+        return 6;
+        if(ch=='P' || ch=='R' || ch=='S')  
+        return 7;
+        if(ch=='T' || ch=='U' || ch=='V')  
+        return 8;
+        if(ch=='W' || ch=='X' || ch=='Y')  
         return 9; 
 }
 int main()
