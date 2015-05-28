445d3d0d83e701f957386a6bc12d57c7b34cf495
王滔 <wwtdsg@gmail.com>
2015-05-28 12:34:07 +0800

    Create PAT (Advanced Level)1001.cpp

---
+++ PAT (Advanced Level)1001.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <complex>
+#include <iomanip>
+
+using namespace std;
+char s[8];
+
+int main()
+{
+  int a, b, y, sum;
+  cin>>a>>b;
+  sum = a + b;
+  if (abs(sum) > 999999)
+  {
+    cout<<sum/1000000<<',';
+    sum = abs(sum) % 1000000;
+    cout<<setfill('0')<<setw(3)<<sum/1000<<','<<setfill('0')<<setw(3)<<sum%1000<<endl;
+  }
+  else if (abs(sum) > 999)
+  {
+    cout<<sum/1000;
+    sum = abs(sum);
+    cout<<','<<setfill('0')<<setw(3)<<sum%1000<<endl;
+  }
+  else
+  {
+    cout<<sum<<endl;
+  }
+}
